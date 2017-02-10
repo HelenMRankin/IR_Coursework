789e1b69a0871387ee196573f434fcdbbb124e06
HelenMRankin <hr80@hw.ac.uk>
2017-02-10 17:29:50 +0000

    Changed state machine

--- Controller.cpp
+++ Controller.cpp
@@ -6,62 +6,136 @@
 #include <yarp/os/all.h>
 #include <yarp/sig/all.h>
 #include <yarp/dev/all.h>
+#include <thread>
+
 using namespace yarp::os;
 using namespace yarp::sig;
 using namespace yarp::dev;
 
-enum State{IDLE, CIRCLE, FACE};
-enum SeeObject{SEE_FACE, SEE_CIRCLE, SEE_NOTHING};
+class Controller {
+public: 
+	Connector* connector;
+	ImageProcessor* processor;
 
-State currentState;
+	enum State { IDLE, CIRCLE, FACE, SPECIAL };
 
-bool canSeeFace() {
-	return false;
-}
+	bool canSeeFace(yarp::sig::Vector* location) {
+		return false;
+	}
 
-bool canSeeCircle() {
-	return false;
-}
+	bool canSeeCircle(yarp::sig::Vector* location) {
+		return false;
+	}
 
-void stateMachine(SeeObject object) {
-	State nextState = IDLE;
-	switch (currentState) {
-	case IDLE: {
-		if (canSeeFace())
-			nextState = FACE;
-		else if (canSeeCircle())
-			nextState = CIRCLE;
+	bool canSeeSpecial() {
+		return false;
 	}
-	case CIRCLE: {
-		if (canSeeFace())
-			nextState = FACE;
-		else if (canSeeCircle())
-			nextState = CIRCLE;
+
+	State transitionFace(yarp::sig::Vector location) {
+
 	}
-	case FACE: {
-		if (canSeeFace())
-			nextState = FACE;
-		else if (canSeeCircle())
-			nextState = CIRCLE;
+
+	State transitionCircle(yarp::sig::Vector location) {
+
 	}
-	default: {
+
+	State transitionSpecial() {
+
 	}
+
+	State transitionIdle() {
+
 	}
-}
+
+	void lookAtLocation(yarp::sig::Vector location) {
+
+	}
+
+	void doSpecial() {
+
+	}
+
+	void stateMachine(State state) {
+		yarp::sig::Vector faceFocus;
+		yarp::sig::Vector circleFocus;
+
+		State nextState = state;
+
+		bool hasFace = canSeeFace(&faceFocus);
+		bool hasCircle = canSeeCircle(&circleFocus);
+		bool hasSpecial = canSeeSpecial();
+		switch (state) {
+		case IDLE: {
+			if (hasSpecial) {
+				nextState = transitionSpecial();
+			}
+			else if (hasFace) {
+				nextState = transitionFace(faceFocus);
+			}
+			else if (hasCircle) {
+				nextState = transitionCircle(circleFocus);
+			}
+			break;
+		}
+		case CIRCLE: {
+			if (hasSpecial) {
+				nextState = transitionSpecial();
+			}
+			else if (hasFace) {
+				nextState = transitionFace(faceFocus);
+			}
+			else if (hasCircle) {
+				lookAtLocation(circleFocus);
+			}
+			else {
+				transitionIdle();
+			}
+			break;
+		}
+		case FACE: {
+			if (hasSpecial) {
+				transitionSpecial();
+			}
+			else if (hasFace) {
+				lookAtLocation(faceFocus);
+			}
+			else {
+				transitionIdle();
+			}
+			break;
+		}
+		case SPECIAL: {
+			if (!hasSpecial) {
+				if (hasFace) {
+					nextState = transitionFace(faceFocus);
+				}
+				else if (hasCircle) {
+					nextState = transitionCircle(circleFocus);
+				}
+				else {
+					transitionIdle();
+				}
+			}
+			break;
+		}
+		default: {
+			break;
+		}
+		}
+
+		stateMachine(nextState);
+	}
+
+	Controller() {
+		connector = new Connector("/test/in", "/icubSim/cam/left");
+		processor = new ImageProcessor();
+		std::thread machine (stateMachine, IDLE);
+	}
+
+};
+
 
 	int main(char ** args) {
-		Connector* connector = new Connector("/test/in","/icubSim/cam/left");
-		ImageProcessor* processor = new ImageProcessor();
-		printf("Got processor - main\n");
+		Controller* controller = new Controller();
 
-		while (1) {
-			printf("Getting image\n");
-			ImageOf<PixelRgb> *source = connector->getImage();
-			printf("Start apply filters");
-			processor->applyFilters(source);
-		}
-			
-
-			char* tmp;
-			std::cin >> tmp;
 	}
